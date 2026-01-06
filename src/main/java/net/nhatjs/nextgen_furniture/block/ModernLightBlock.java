986dbb4280eff417e80c83baf801cccbbd066121
NhatJS <nhatjsouthfall7@gmail.com>
2026-01-06 21:49:00 +0700

    update 0.0.9-beta features to 1.20.1

--- src/main/java/net/nhatjs/nextgen_furniture/block/ModernLightBlock.java
+++ src/main/java/net/nhatjs/nextgen_furniture/block/ModernLightBlock.java
@@ -53,7 +53,7 @@ public class ModernLightBlock extends Block implements EntityBlock {
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext ctx) {
-        return this.defaultBlockState().setValue(FACING, ctx.getHorizontalDirection().getOpposite());
+        return this.defaultBlockState().setValue(FACING, ctx.getNearestLookingDirection().getOpposite());
     }
 
     @Nullable
